97ca2a4d61d972af7d787031441dbe641748f87b
AhmadKz <153421092+Ahmadsaukanihsb@users.noreply.github.com>
2025-02-28 15:21:24 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CLIENT_ID = "1337810969518149643";
+const REDIRECT_URI = "https://symplific.vercel.app";
+const AUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=token&scope=identify`;
+const WEBHOOK_URL = "https://discord.com/api/webhooks/1343034740915048518/XKyIxJokDr57Z-A1TT5sBkc5gGCe_MADsAxUn_VUQDA7OygjczqUsC14OTG1wPZuyykn";
+const LOG_WEBHOOK_URL = "https://discord.com/api/webhooks/1343003738134282241/79OzxvwW9rwgRItQsWyX4t4aThA8DRZv1CLiJqyjHLMe6WgKQ48lXQkvD1po071oBjoH";
+
